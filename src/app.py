a23001331e8771bc209e4caf354267717db02069
MyNameIsGs <sebascarmun@gmail.com>
2023-04-05 20:49:59 +0000

    Homework done

--- src/app.py
+++ src/app.py
@@ -6,39 +6,60 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
 
-    # this is how you can use the Family datastructure by calling its methods
+
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
 
+    return jsonify(members), 200
 
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member is None:
+        return jsonify({"Message" : "No matching members found"}), 404
+
+    return jsonify(member), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.json
+    jackson_family.add_member(body)
+    members = jackson_family.get_all_members()
+    
+    response_body = { "family": members }
+    
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    members = jackson_family.get_all_members()
+    member = jackson_family.get_member(id)
+    if member is None:
+        return jsonify({"Message" : "Bad Request"}), 400
+    jackson_family.delete_member(id)
+    return jsonify({ "done" : True }), 200
+    
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
